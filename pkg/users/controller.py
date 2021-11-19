bfee6f54a1818bf199fd5fd2a222d922c245f802
Hong Xiang <hongxiang17@gmail.com>
2021-11-18 22:17:37 -0300

    Add initial project

---
+++ pkg/users/controller.py
@@ -0,0 +1,18 @@
+from .model import modelUsers
+from bson.json_util import dumps
+
+
+class usersController:
+    model = None
+    def getUserById(self,id:int):
+        result = self.model.getUserById(id)
+        return dumps(result)
+    
+    def findUserByField(self,field:str,value):
+        result = self.model.findUserByField(field,value)
+        for element in result:
+           element = dumps(element)
+        return dumps(result)
+
+    def __init__(self):
+        self.model = modelUsers()
