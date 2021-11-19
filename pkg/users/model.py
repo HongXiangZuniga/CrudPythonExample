450192ef031c7a9907d31367adb634dddb582fb2
Hong Xiang <hongxiang17@gmail.com>
2021-11-19 19:25:00 -0300

    delete objectId to response

--- pkg/users/model.py
+++ pkg/users/model.py
@@ -8,12 +8,14 @@ class modelUsers:
 
     def getUserById(self,id:int):
         result = self.collection.find_one({"id":id})
+        result.pop('_id', None)
         return result
     
     def findUserByField(self,field:str,value):
         result = []
         mongoResult = self.collection.find({field:value})
         for element in mongoResult:
+            element.pop('_id', None)
             result.append(element)
             mongoResult.next()
         if len(result)==0:
